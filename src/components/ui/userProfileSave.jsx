cba851878bd8c6c75ede4b5305611ab370884fb7
theshoxjaxon <theshoxjaxon@gmail.com>
2026-02-18 17:13:29 +0500

    Added New Branch

---
+++ src/components/ui/userProfileSave.jsx
@@ -0,0 +1,11 @@
+
+const userProfileSave = () => {
+  return (
+    <div className="user-profile-save">
+      <h2>Save User Profile</h2>
+      <p>Your changes have been saved successfully!</p>
+    </div>
+  )
+}
+
+export default userProfileSave
